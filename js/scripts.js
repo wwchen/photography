9eb108b9f205067b1a8c0b36fa67673527034d35
William Chen <williamchen16@gmail.com>
2015-04-29 09:10:58 -0700

    Realized this is template is written pretty shittily.. Fixing scrolling anchor bugs

--- js/scripts.js
+++ js/scripts.js
@@ -1,78 +1,43 @@
+$(function() {
+  /* highlight the top nav as scrolling occurs */
+  $('body').scrollspy({
+    target: '#nav',
+    offset: $('#nav').height()
+  })
 
-$(document).ready(function(){/* affix the navbar after scroll below header */
-$('#nav').affix({
-      offset: {
-        top: $('header').height()-$('#nav').height()
-      }
-});	
+  /* affix the navbar after scroll below header */
+  $('#nav').affix({
+    offset: {
+      top: $('header').height() - $('#nav').height() - 1
+    }
+  });	
 
-/* highlight the top nav as scrolling occurs */
-$('body').scrollspy({ target: '#nav' })
+  /* smooth scrolling for nav sections */
+  $('#nav .navbar-nav li>a').click( function(e) {
+    e.preventDefault();
+    var link = $(this).attr('href');
+    var posi = 0;
+    var headerOffset = $('header').height() - $(window).scrollTop();
+    if (headerOffset > 0) {
+      posi = headerOffset - $("#nav").height();
+      posi = $(link).offset().top - $("#nav").height();
+      console.log("there");
+    }
+    else {
+      posi = $(link).offset().top - $("#nav").height();
+      console.log("here");
+    }
+    console.log(posi);
+    $('html, body').animate({scrollTop: posi}, 700);
+  })
 
-/* smooth scrolling for scroll to top */
-$('.scroll-top').click(function(){
-  $('body,html').animate({scrollTop:0},1000);
-})
-
-/* smooth scrolling for nav sections */
-$('#nav .navbar-nav li>a').click(function(){
-  var link = $(this).attr('href');
-  var posi = $(link).offset().top+20;
-  $('body,html').animate({scrollTop:posi},700);
-})
-
-/* google maps */
-
-// enable the visual refresh
-google.maps.visualRefresh = true;
-
-var map;
-function initialize() {
-  var mapOptions = {
-    zoom: 15,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  };
-  map = new google.maps.Map(document.getElementById('map-canvas'),
-      mapOptions);
-  	// try HTML5 geolocation
-  if(navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      var pos = new google.maps.LatLng(position.coords.latitude,
-                                       position.coords.longitude);
-
-      var infowindow = new google.maps.InfoWindow({
-        map: map,
-        position: pos,
-        content: 'Location found using HTML5.'
-      });
-
-      map.setCenter(pos);
-    }, function() {
-      handleNoGeolocation(true);
-    });
-  } else {
-    // browser doesn't support geolocation
-    handleNoGeolocation(false);
-  }
-}
-
-function handleNoGeolocation(errorFlag) {
-  if (errorFlag) {
-    var content = 'Error: The Geolocation service failed.';
-  } else {
-    var content = 'Error: Your browser doesn\'t support geolocation.';
-  }
-
-  var options = {
-    map: map,
-    position: new google.maps.LatLng(60, 105),
-    content: content
-  };
-
-  var infowindow = new google.maps.InfoWindow(options);
-  map.setCenter(options.position);
-}
-google.maps.event.addDomListener(window, 'load', initialize);
-
+  /* instafeed.js */
+  var feed = new Instafeed({
+    get: 'user',
+    userId: 1416158121,
+    accessToken: '1416158121.467ede5.6a670872bf694751b43af736bdd775cf',
+    template: '<a href="{{link}}"><img src="{{image}}"></a>'
+  });
+  feed.run();
 
 });
